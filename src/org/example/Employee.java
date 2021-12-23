f84b321cee0b2ca19615a0d18c3806b0fc09eb87
Anitha-12345678 <mirnicse@gmail.com>
2021-12-23 11:52:34 +0530

    first task

---
+++ src/org/example/Employee.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Employee {
+	
+
+		
+		
+		public void empID() {
+			System.out.println("123");
+		}
+		
+		
+		public void empName() {
+		System.out.println("vino");
+	}
+
+		
+	}
+
+
+			
